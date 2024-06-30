75fa0ea065cd65e7611ede343dc19380aeaa9f40
tomoEng11 <tom_friendship@me.com>
2024-06-30 23:10:14 +0900

    天気画面への遷移を実装

--- 7_Clima-iOS13/Clima/CityListView/Controller/CityListViewController.swift
+++ 7_Clima-iOS13/Clima/CityListView/Controller/CityListViewController.swift
@@ -13,4 +13,9 @@ class CityListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "WeatherViewController") as! WeatherViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
